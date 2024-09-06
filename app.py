dfae068c11122d8cbe993f21254b5e87120f6beb
Christian Kotyrba <CKotyrba@gmx.de>
2024-09-06 23:06:53 +0200

    add product price endpoint

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 from flask import Flask, render_template
 
+import price_calculator
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
     return render_template("index.html")
+
+@app.route('/product/<productId>/price', methods=['GET'])
+def get_product_price(productId):
+    return str(price_calculator.price(productId)),200
+
 
 if __name__ == "__main__":
     app.run()
